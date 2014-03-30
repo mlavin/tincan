1687fef2db1cfec66f74bf87c884622d4926e77b
mlavin <markdlavin@gmail.com>
2014-03-30 15:50:41 -0400

    Almost working RTC connection...

--- js/views.js
+++ js/views.js
@@ -1,4 +1,4 @@
-var views = (function ($, _, Backbone) {
+var views = (function ($, _, Backbone, getUserMedia, Offer) {
     var HomePageView = Backbone.View.extend({
         el: '#homepage',
         events: {
@@ -27,6 +27,7 @@ var views = (function ($, _, Backbone) {
             this.options = options;
             this.server = this.options.server;
             this.server.on('new-peer', _.bind(this.onPeer, this));
+            this.server.on('peer-msg', _.bind(this.onSignalMessage, this));
             this.template = _.template($(this.template).html());
         },
         render: function () {
@@ -36,6 +37,27 @@ var views = (function ($, _, Backbone) {
         },
         onPeer: function () {
             $('#peer-status', this.$el).text('Connected').addClass('active');
+            getUserMedia({audio:true, video:false, fake: true},
+                _.bind(this.gotLocalStream, this), _.bind(this.reportFailure, this));
+        },
+        gotLocalStream: function (stream) {
+            var self = this;
+            this.offer = new Offer(this.server.room, stream);
+            this.offer.on('local-offer', function (offerDesc) {
+                self.server.send(JSON.stringify(offerDesc));
+            });
+        },
+        reportFailure: function () {
+            //
+        },
+        connectionOpen: function () {
+            console.log('Connection open!');
+        },
+        onSignalMessage: function (msg) {
+            if (this.offer && !this.answer) {
+                // Assume the first message is the answer
+                this.offer.acceptAnswer(JSON.parse(msg));
+            }
         }
     }),
     PeerView = Backbone.View.extend({
@@ -46,6 +68,7 @@ var views = (function ($, _, Backbone) {
             this.server = this.options.server;
             this.room = this.options.room;
             this.template = _.template($(this.template).html());
+            this.server.on('peer-msg', _.bind(this.onSignalMessage, this));
         },
         render: function () {
             this.$el.html(this.template());
@@ -56,10 +79,29 @@ var views = (function ($, _, Backbone) {
             this.$el.addClass('active');
         },
         validRoom: function () {
-
+            var self = this;
+            getUserMedia({audio:true, video:false, fake: true},
+                _.bind(this.gotLocalStream, this), _.bind(this.reportFailure, this));
         },
         invalidRoom: function () {
 
+        },
+        reportFailure: function () {
+
+        },
+        gotLocalStream: function (stream) {
+            var self = this;
+            self.offer = new Offer(this.server.room, stream);
+            self.offer.on('local-answer', function (answerDesc) {
+                console.log('answer');
+                self.server.send(JSON.stringify(answerDesc));
+            });
+        },
+        onSignalMessage: function (msg) {
+            if (this.offer && !this.answer) {
+                // Assume the first message is an offer
+                this.offer.acceptOffer(JSON.parse(msg));
+            }
         }
     });
     return {
@@ -67,4 +109,4 @@ var views = (function ($, _, Backbone) {
         leader: LeaderView,
         peer: PeerView,
     }
-})(jQuery, _, Backbone);
+})(jQuery, _, Backbone, getUserMedia, Offer);
