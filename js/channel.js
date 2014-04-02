0bc63d746b1f02a20fe1dfbc0dc0fbfd638a2c47
mlavin <markdlavin@gmail.com>
2014-04-01 20:48:51 -0400

    Working in Firefox.

--- js/channel.js
+++ js/channel.js
@@ -1,32 +1,48 @@
-var Channel = (function ($, _, Backbone, webrtcDetectedBrowser, getUserMedia, attachMediaStream,
+var Channel = (function ($, _, Backbone, webrtcDetectedBrowser, getUserMedia,
     RTCPeerConnection, RTCSessionDescription, RTCIceCandidate) {
     var servers = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]},
-        connectionOptions = {optional: [{RtpDataChannels: true}]};
+        connectionOptions = {optional: [{RtpDataChannels: true}]},
+        isChrome = webrtcDetectedBrowser === 'chrome',
+        isFirefox = webrtcDetectedBrowser === 'firefox',
+        offerOptions = {
+            optional: [],
+            mandatory: {
+                OfferToReceiveAudio: isFirefox,
+                OfferToReceiveVideo: isFirefox
+            }
+        };
 
     function Channel(signal, leader) {
         var self = this;
         this.signal = signal;
         this.leader = leader;
         this.signal.on('peer-msg', _.bind(this.onSignalMessage, this));
-        if (webrtcDetectedBrowser === 'firefox') {
-            // Need to fetch media stream
-            getUserMedia({audio:true, video:false, fake: true}, function (stream) {
-                self.peer.addStream(stream);
-            }, _.bind(this.onError, this));
-        }
+        
         this.peer = new RTCPeerConnection(servers, connectionOptions);
         this.peer.onicecandidate = _.bind(this.onICECandiate, this);
-        this.channel = this.peer.createDataChannel(this.signal.room, {reliable: false});
-        this.channel.onmessage = _.bind(this.onChannelMessage, this);
-        this.channel.onopen = _.bind(this.onChannelOpen, this);
-        this.channel.onerror = _.bind(this.onError, this);
+        this.peer.ondatachannel = _.bind(this.onDataChannel, this);
+        this.channel = this.peer.createDataChannel(this.signal.room, isChrome ? {reliable: false} : {});
+        this.onDataChannel({channel: this.channel});
         this.ready = new $.Deferred();
         this.events = _.extend({}, Backbone.Events);
         if (this.leader) {
-            this.peer.createOffer(
-                _.bind(this.onlocalDescription, this),
-                _.bind(this.onError, this)
-            );
+            this.stream = new $.Deferred();
+            if (isFirefox) {
+                // Need to fetch media stream
+                getUserMedia({audio: true, video: false, fake: true}, function (stream) {
+                    self.peer.addStream(stream);
+                    self.stream.resolve(true);
+                }, _.bind(this.onError, this));
+            } else {
+                this.stream.resolve(true);
+            }
+            this.stream.done(function () {
+                self.peer.createOffer(
+                    _.bind(self.onlocalDescription, self),
+                    _.bind(self.onError, self),
+                    offerOptions
+                );
+            });
         }        
     }
 
@@ -52,7 +68,8 @@ var Channel = (function ($, _, Backbone, webrtcDetectedBrowser, getUserMedia, at
                 if (self.peer.remoteDescription.type === 'offer') {
                     self.peer.createAnswer(
                         _.bind(self.onlocalDescription, self),
-                        _.bind(self.onError, this)
+                        _.bind(self.onError, this),
+                        offerOptions
                     );
                 }
             }, _.bind(this.onError, this));
@@ -76,6 +93,13 @@ var Channel = (function ($, _, Backbone, webrtcDetectedBrowser, getUserMedia, at
         }
     };
 
+    Channel.prototype.onDataChannel = function (e) {
+        this.channel = e.channel;
+        this.channel.onmessage = _.bind(this.onChannelMessage, this);
+        this.channel.onopen = _.bind(this.onChannelOpen, this);
+        this.channel.onerror = _.bind(this.onError, this);
+    };
+
     Channel.prototype.onChannelOpen = function () {
         this.ready.resolve(this.channel);
     };
@@ -91,5 +115,5 @@ var Channel = (function ($, _, Backbone, webrtcDetectedBrowser, getUserMedia, at
     };
 
     return Channel;
-})($, _, Backbone, webrtcDetectedBrowser, getUserMedia, attachMediaStream,
+})($, _, Backbone, webrtcDetectedBrowser, getUserMedia,
     RTCPeerConnection, RTCSessionDescription, RTCIceCandidate);
