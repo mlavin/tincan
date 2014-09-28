ed2daa0868c1fb0d1b9f41d008fdc65e8bf3545c
mlavin <markdlavin@gmail.com>
2014-09-28 10:14:09 -0400

    Testing socket information in the responses.

--- shoestring/tests/test_handlers.py
+++ shoestring/tests/test_handlers.py
@@ -1,6 +1,9 @@
 import json
 
+from contextlib import contextmanager
 from unittest.mock import patch
+
+import jwt
 
 from tornado.testing import AsyncHTTPTestCase, LogTrapTestCase
 
@@ -11,7 +14,20 @@ class BaseAppTestCase(AsyncHTTPTestCase, LogTrapTestCase):
     """Base class for testing ShoestringApplication and handlers."""
 
     def get_app(self):
-        return ShoestringApplication()
+        return ShoestringApplication(secret='XXXX')
+
+    def assertJSON(self, response, expected_status=200):
+        """Return context manager for asserting JSON responses."""
+        self.assertEqual(response.code, expected_status)
+        try:
+            result = json.loads(response.body.decode('utf-8'))
+        except:
+            self.fail('Received non-JSON response.')
+        else:
+            @contextmanager
+            def json_context():
+                yield result
+        return json_context()
 
 
 class RoomHandlerTestCase(BaseAppTestCase):
@@ -23,9 +39,24 @@ class RoomHandlerTestCase(BaseAppTestCase):
         mock_create.return_value = '1234'
         response = self.fetch('/rooms', method='POST', body='')
         self.assertTrue(mock_create.called)
-        self.assertEqual(response.code, 200)
-        result = json.loads(response.body.decode('utf-8'))
-        self.assertEqual(result['room'], '1234')
+        with self.assertJSON(response) as result:
+            self.assertEqual(result['room'], '1234')
+
+    @patch('shoestring.backends.memory.Backend.create_room')
+    def test_new_room_socket(self, mock_create):
+        """Creating a room should return info to connect to the websocket."""
+        mock_create.return_value = '1234'
+        response = self.fetch('/rooms', method='POST', body='')
+        with self.assertJSON(response) as result:
+            protocol = 'ws' if self.get_protocol() == 'http' else 'wss'
+            expected = '{}://localhost:{}/socket'.format(protocol, self.get_http_port())
+            self.assertEqual(result['socket'], expected)
+            self.assertIn('user', result)
+            self.assertIn('token', result)
+            user, token = result['user'], result['token']
+            info = jwt.decode(token, 'XXXX')
+            self.assertEqual(info['uuid'], user)
+            self.assertEqual(info['room'], '1234')
 
     @patch('shoestring.backends.memory.Backend.join_room')
     def test_existing_room(self, mock_join):
@@ -33,9 +64,25 @@ class RoomHandlerTestCase(BaseAppTestCase):
         mock_join.return_value = '1234'
         response = self.fetch('/rooms/1234', method='GET')
         self.assertTrue(mock_join.called)
-        self.assertEqual(response.code, 200)
-        result = json.loads(response.body.decode('utf-8'))
-        self.assertEqual(result['room'], '1234')
+        with self.assertJSON(response) as result:
+            self.assertEqual(result['room'], '1234')
+
+    @patch('shoestring.backends.memory.Backend.join_room')
+    def test_join_room_socket(self, mock_join):
+        """Joining a room should return info to connect to the websocket."""
+        mock_join.return_value = '1234'
+        response = self.fetch('/rooms/1234', method='GET')
+        self.assertTrue(mock_join.called)
+        with self.assertJSON(response) as result:
+            protocol = 'ws' if self.get_protocol() == 'http' else 'wss'
+            expected = '{}://localhost:{}/socket'.format(protocol, self.get_http_port())
+            self.assertEqual(result['socket'], expected)
+            self.assertIn('user', result)
+            self.assertIn('token', result)
+            user, token = result['user'], result['token']
+            info = jwt.decode(token, 'XXXX')
+            self.assertEqual(info['uuid'], user)
+            self.assertEqual(info['room'], '1234')
 
     @patch('shoestring.backends.memory.Backend.join_room')
     def test_missing_room(self, mock_join):
